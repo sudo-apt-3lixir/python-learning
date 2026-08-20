3f3e49f767522dcfe2d8b6d86b1243ceea47d792
3lixir <oliwer.wiberg@outlook.com>
2026-08-20 22:36:45 +0200

    Add grade calculator script

---
+++ 01-basics/grade_calculator.py
@@ -0,0 +1,16 @@
+print("=== Grade Calculator ===")
+
+score = int(input("Enter your score (0-100): "))
+
+if score >= 90:
+    grade = "A"
+elif score >= 80:
+    grade = "B"
+elif score >= 70:
+    grade = "C"
+elif score >= 60:
+    grade = "D"
+else:
+    grade = "F"
+
+print("Your grade is:", grade)
